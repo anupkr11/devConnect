17ee82b3bf71165a398a3bb251c0d0f24b4bdd88
anupkr11 <anup94622@gmail.com>
2026-01-30 12:11:23 +0530

    created server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req,res) =>{
+    res.send("Hello, World!");
+})
+
+app.get("/test", (req,res) =>{
+    res.send("Welcome to the test route!");
+})
+
+app.get("/hello", (req,res) =>{
+    res.send("Hello from the /hello route!");
+})
+
+app.listen(3000, () =>{
+    console.log("Server is running on port 3000");
+})
