4ef56fe4867fc365ae754c6a061921b20a804f79
anupkr11 <anup94622@gmail.com>
2026-01-30 12:47:32 +0530

    testing routes

--- src/app.js
+++ src/app.js
@@ -2,17 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.get("/", (req,res) =>{
-    res.send("Hello, World!");
+//this get will match only get http method
+app.get("/user", (req,res) =>{
+    res.send({ name: "John Doe", age: 30 });
 })
 
-app.get("/test", (req,res) =>{
-    res.send("Welcome to the test route!");
+app.post("/user", (req,res) =>{
+    res.send("User created successfully!");
 })
 
-app.get("/hello", (req,res) =>{
-    res.send("Hello from the /hello route!");
+app.get("/product/:id", (req,res) =>{
+    console.log(req.params.id); 
+    res.send({ id: req.params.id, name: "Laptop", price: 999.99 });
 })
+
+// app.get("/test", (req,res) =>{
+//     res.send("Welcome to the test route!");
+// })
+
+//this use will match all the http methods
+// app.use("/hello", (req,res) =>{
+//     res.send("Hello from the /hello route!");
+// })
 
 app.listen(3000, () =>{
     console.log("Server is running on port 3000");
