62f08d79524cc8ba37e87db7b1e1e358ab2901f4
anupkr11 <anup94622@gmail.com>
2026-02-05 11:03:26 +0530

    data sanitization

--- src/app.js
+++ src/app.js
@@ -45,11 +45,28 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  const userId =  req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId =  req.params?.userId;
   const data = req.body;
   try{
-    const user = await User.findByIdAndUpdate({_id: userId}, data);
+
+    const UpdatedData = ["gender", "skills", "photoURL", "about"];
+    const isAllowedUpdates = Object.keys(data).every((k)=>{
+      UpdatedData.includes(k);
+    })
+
+    if(!isAllowedUpdates){
+      throw new Error("Invalid updates!");
+    }
+
+    if(data?.skills.length > 10){
+      throw new Error("Skills cannot be more than 10!");
+    }
+
+    const user = await User.findByIdAndUpdate({_id: userId}, data, {
+      returnDocument: "after",
+      runValidators: true
+    });
     res.send("user updated successfully");
   }
   catch(err){
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,67 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    firstName:{
-        type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minlength: 4,
+      maxlength: 30,
     },
-    lastName:{
-        type: String,
+    lastName: {
+      type: String,
     },
-    email:{ 
-        type: String,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(val){
+        if(!validator.isEmail(val)){
+          throw new Error("Invalid email");
+        }
+      }
     },
-    password:{ 
-        type: String,
+    password: {
+      type: String,
+      required: true,
+      validate(val){
+        if(!validator.isStrongPassword(val)){
+          throw new Error("Enter a strong password");
+        }
+      }
     },
-    age:{
-        type: Number,
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
     },
     gender: {
-        type: String,
-    }
-})
+      type: String,
+      validate(val) {
+        if (!["male", "female", "other"].includes(val)) {
+          throw new Error("Gender is not valid");
+        }
+      },
+    },
+    skills: [String],
+    photoURL: {
+      type: String,
+      validate(val){
+        if(!validator.isURL(val)){
+          throw new Error("Invalid photo URL");
+        }
+      }
+    },
+    about: {
+      type: String,
+      default: "This is the about section.",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema);
