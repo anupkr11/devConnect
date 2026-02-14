56cc64073653125cb8f11449afd0725054b4ad51
anupkr11 <anup94622@gmail.com>
2026-02-14 12:13:02 +0530

    user connections intereactions

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,23 +1,51 @@
-const express = require('express');
+const express = require("express");
 const userRouter = express.Router();
-const {userAuth} = require("../middlewares/auth");
+const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
 
-userRouter.get("/user/requests/recieved", userAuth, async (req, res) =>{
-    try{
-        const loggedInUser = req.user;
-        const connectionReq = await ConnectionRequestModel.find({
-            toUserId: loggedInUser._id,
-            status: "interested"
-        }).populate("fromUserId", ["firstName", "lastName", "email", "skills", "photoURL"]);
+const USER_DATA_FIELDS = "firstName lastName email skills photoURL";
 
-        res.json({ message: "Received connection requests fetched successfully", data: connectionReq });
 
-    }
-    catch(err){
-        res.status(400).send("Error fetching received connection requests: " + err.message);
-    }
-})
+userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionReq = await ConnectionRequestModel.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate("fromUserId", USER_DATA_FIELDS);
 
+    res.json({
+      message: "Received connection requests fetched successfully",
+      data: connectionReq,
+    });
+  } catch (err) {
+    res
+      .status(400)
+      .send("Error fetching received connection requests: " + err.message);
+  }
+});
+
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionReq = await ConnectionRequestModel.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_DATA_FIELDS)
+      .populate("toUserId", USER_DATA_FIELDS);
+    const data = connectionReq.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send("Error fetching connections: " + err.message);
+  }
+});
 
 module.exports = userRouter;
