dd43eff08ca1f76bceb62089d4f49f7fd9393f94
anupkr11 <anup94622@gmail.com>
2026-02-14 12:55:26 +0530

    handled feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,9 +2,9 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const USER_DATA_FIELDS = "firstName lastName email skills photoURL";
-
 
 userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   try {
@@ -45,6 +45,35 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({ data });
   } catch (err) {
     res.status(400).send("Error fetching connections: " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    // User should see all the user cards except
+    // 0. his own card
+    // 1. his connections
+    // 2. ignored people
+    // 3. already sent the connection request
+    const loggedInUser = req.user;
+    const connectionReq = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    const hideUsersFromFeed = new Set();
+    connectionReq.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $ne: loggedInUser._id } },
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+      ],
+    }).select(USER_DATA_FIELDS);
+    res.json({ data: users });
+  } catch (err) {
+    res.status(400).send("Error fetching feed: " + err.message);
   }
 });
 
