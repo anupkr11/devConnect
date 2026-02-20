9cb113d3f405a0edb19611107d943352347991a1
anupkr11 <anup94622@gmail.com>
2026-02-20 11:04:01 +0530

    update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,10 +9,10 @@ const { validateData } = require("../utils/validate");
 authRouter.post("/signup", async (req, res) => {
   try {
     validateData(req);
-    const { firstName, lastName, email, password, about, photoURL } = req.body;
+    const { firstName, lastName, email, password, about, photoURL, age, gender } = req.body;
     const passHash = await bcrypt.hash(password, 10);
     const user = new User(
-      { firstName, lastName, email, password: passHash, about, photoURL },
+      { firstName, lastName, email, password: passHash, about, photoURL, age, gender },
       // firstName: "John",
       // lastName: "Doe",
       // email: "john.doe@example.com",
